1a240f11e0bf64b3a90dba21d0401f9b324ca60c
Alfredo Vidueira <vidueira1992@gmail.com>
2024-12-27 17:17:43 -0300

    chore: init.sh script updated

--- init.sh
+++ init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
 if [ -z "$1" ]; then
   echo "Usage: $0 go-starter"
   exit 1
 fi
-
-replacement="$1"
-
-script_name=$(basename "$0")
-
-find . -type f ! -name "$script_name" -exec sed -i '' "s/go-starter/$replacement/g" {} +
-
+git clone https://github.com/avidueiram/go-starter.git $1
+cd $1
+rm -rf .git
+git init
+git branch -M master
+git add .
+git commit -m "feat: initial commit"
+npm i
+echo "Project $1 initialized in $(pwd)"
+find . -type f ! -name "$(basename "$0")" -exec sed -i '' "s/go-starter/$1/g" {} +
 echo "Project $1 initialized in $(pwd)"
